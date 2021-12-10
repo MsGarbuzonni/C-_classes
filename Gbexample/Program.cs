e6193cc6ee1c337ed35a0890fb29b8e60ec976d9
Elena Garbuz <ms.garbuzonni@gmail.com>
2021-12-10 11:50:50 +0300

    повторение первой лекции. Сохранение 2

--- Gbexample/Program.cs
+++ Gbexample/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// вывести квадрат числа
+// byte (8 бит занимает)
+// shart (16 бит)
+// int (32 бит)
+// long (64 бита)
+
+// тип_данных имя_переменной = значение; 
+int myint = 10;
+Console.WriteLine(myint);
+
+// float
+// double 
+double mydouble = 2.14;
+Console.WriteLine(mydouble);
+// char - символьный 
+char myChar = 'a';
+Console.WriteLine(myChar);
+
+// bool  - логический тип данных (истина\ложь)
+bool myBool = true;
+Console.WriteLine(myBool);
+bool myFolce = false;
+Console.WriteLine(myFolce);
+
+
+// string - строчка, массив данных 
+
+string myString = "это строка";
+Console.WriteLine(myString);
+
+// + - * / %
+int first = 9;
+int second = 10;
+int third = (first + second) * 5;
+Console.WriteLine(third);
+
+// if (условие)
+//{
+// тело_из_набора_оператораж   
+//}
+
+// < > >= <= == !=
+// && || !- логическое отрицание
+
+if (first < second)
+{
+    Console.WriteLine(first +"<"+second);
+}
+else if (first>second)
+{
+    Console.WriteLine(first+">"+second);
+}
+else
+{
+    Console.WriteLine("по умолчанию");
+}
+
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(a);
+
+double b = Math.Sqrt(9);
+Console.WriteLine(b); 
